1524589865842d8988758364c0e43277cb1edc8f
dawidratajczyk <59934287+dawidratajczyk@users.noreply.github.com>
2020-07-26 20:34:49 +0200

    CleaningTestFiles

--- rodo/src/main/java/pl/mmstomatologia/classes/repository/crudRodo.java
+++
@@ -1,9 +0,0 @@
-package pl.mmstomatologia.classes.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import pl.mmstomatologia.classes.entities.rodo;
-
-public interface crudRodo extends JpaRepository<rodo, Long>{
-
-}
